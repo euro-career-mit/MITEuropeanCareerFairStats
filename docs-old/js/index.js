81d290ddaf8bad972593083c6d1c9a959bc43451
dogadogan <mdogadogan@gmail.com>
2020-05-10 00:05:20 -0400

    new layout

--- docs-old/js/index.js
+++ docs-old/js/index.js
@@ -1,7 +1,7 @@
 var vizSignupNumbers, vizSchools, vizStackedMajors, vizHighestDegrees, vizMajors, vizCompanyMap;
 
 // Load 2020 data
-fetch("data/FP_Candidate_Groups_All.json") // A4_Groups_All.json
+fetch("A4_Groups_All.json") // A4_Groups_All.json
 .then(response => response.json())
 .then(json => {
 
@@ -16,7 +16,7 @@ fetch("data/FP_Candidate_Groups_All.json") // A4_Groups_All.json
 
 
 // Load signup numbers for all years
-fetch("data/FP_Candidate_signups.json")
+fetch("ECF_signups.json")
 .then(response => response.json())
 .then(json => {
 
@@ -26,21 +26,11 @@ fetch("data/FP_Candidate_signups.json")
 
 
 // Load company data
-fetch("data/FP_Employers_All.json")
+fetch("A4_Employers_20.json")
 .then(response => response.json())
 .then(json => {
 
     drawCompanyMap(json);
-
-});
-
-
-// Load company signup numbers for all years
-fetch("data/FP_Employers_signups.json")
-.then(response => response.json())
-.then(json => {
-
-    drawCompanySignupNumbers(json);
 
 });
 
@@ -161,17 +151,13 @@ drawStackedMajors = (data) => {
       if(d["University"]=="Other"){ return false; } // do not show "Other"
       return true; 
     })
+    .height(800)
     .shapeConfig({
       labelConfig: {
-        fontSize: 16 // this effects the legend
+        fontFamily: "Poppins",
+        fontMax: 100
       }
     })
-    // .legendConfig({
-    //   labelConfig: {
-    //     fontSize: 16
-    //   }
-    // })
-    .height(800)
     .render();
 
 }
@@ -246,7 +232,7 @@ drawHighestDegrees = (data) => {
     })
     .time("Year")
     .legend(false)
-    .height(400)
+    .height(240)
     .render();
 
 }
@@ -265,7 +251,7 @@ drawCompanyMap = (data) => {
     .select("#divCompanyMap")
     .data(data)
     .colorScale("Number of Records")
-    .topojson("data/countries-50m.json")
+    .topojson("countries-50m.json")
     //.topojsonId("id")
     // .tooltipConfig({
     //   footer: function(d) {
@@ -296,74 +282,12 @@ drawCompanyMap = (data) => {
     // .fitFilter(function(d) { console.log(d["id"]);
     //   return ["840", "250"].indexOf(d["id"]) < 0;
     // })
-    .time("Year")
-    .on("click", function(d) {
-      showCountryCompanies(d["Country"]);
-      console.log(d["Country"]);
-      //alert((d["Country"]); + " has been clicked!");
-    })
-    // .on("hover", function(d) {
-    //   console.log(d["Country"] + " has been hovered!");
-    // })
     .height(600)
     .render();
 
     // https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json
 
 }
-
-
-// first hide the elements when the document is lodaded
-$( document ).ready(function() {
-    //hide all cards
-    $('.country-card').hide();
-    //show the welcome card
-    $('#start-countrycard').show();
-});
-
-
-function showCountryCompanies(countryName) {
-    //Hide all cards
-    $('.country-card').removeClass('scale-in').addClass('scale-out').hide();
-
-    // now only show the clicked country!
-    $('#'+countryName).show().removeClass('scale-out').addClass('scale-in');
-}
-
-
-
-// drawCompanySignupNumbers function call
-drawCompanySignupNumbers = (data) => {
-
-  vizSignupNumbers = new d3plus.BarChart()  // LinePlot()
-    .select("#divCompanySignupNumbers")
-    .data(data)
-    .y("Number of Organizations")
-    .x("Year")
-    .groupBy("Number of Organizations")
-    .legend(false)
-    .tooltip(false)
-    //.groupBy("Major", "University")
-    // .ySort(function(a, b) { return UniversityList.indexOf(b["University"]) - UniversityList.indexOf(a["University"]); })
-    // .stacked(true)
-    // .tooltipConfig({
-    //     title: function(d) {
-    //       return d["University)"];
-    //     },
-    //     tbody: [
-    //       ["Field of study:", function(d) { return d["Major"] }],
-    //       ["Number of candidates:", function(d) { return d["Number of Records"] }]  // return d.year
-    //     ]
-    //   })
-    // .time("Year")
-    //.time({"value": "Year", "size":10})
-    //.time({"fixed":true})
-    //.timeline({"hover":"grab"})
-    .height(200)
-    .render();
-
-}
-
 
 
 
